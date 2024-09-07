272420e5229f24f01a0a559a2d1c17c76461a109
kumarnaveenkamat <146153015+kumarnaveenkamat@users.noreply.github.com>
2024-09-07 11:59:12 +0530

    Revert "Add files via upload"

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
